78baf702585a56af9af943ba2aadc64c4a0af3e4
Yatharth Ahuja <yatharthahuja1999@gmail.com>
2023-07-12 14:24:08 +0530

    new file

---
+++ fundamentalDataTypes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+	signed int a{256};
+	bool b{true};
+	std::cout<<a<<'\n';
+	std::cout<<b<<'\n';
+	std::cout<<std::endl;
+	// string, arrays, 2d arrays, chars
+	return 0;
+}
